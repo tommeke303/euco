7ddbb4c3d248b60ee9a579f0e26d6f8ed3a95562
tommeke303 <Thomas@pcThomas.local>
2017-08-28 13:02:47 +0200

    menu+footer

--- EUCO2018/include/sendemail.php
+++ EUCO2018/include/sendemail.php
@@ -14,12 +14,12 @@ if( isset( $_POST['template-contactform-submit'] ) AND $_POST['template-contactf
         $subject = $_POST['template-contactform-subject'];
         $message = $_POST['template-contactform-message'];
 
-        $subject = isset($subject) ? $subject : 'New Message From Contact Form';
+        $subject = isset($subject) ? $subject : 'Nieuw bericht van het contact formulier';
 
         $botcheck = $_POST['template-contactform-botcheck'];
 
-        $toemail = 'semicolonweb@gmail.com'; // Your Email Address
-        $toname = 'SemiColon'; // Your Name
+        $toemail = 'Info@euco2018.be'; // Your Email Address
+        $toname = 'EUCO 2018'; // Your Name
 
         if( $botcheck == '' ) {
 
@@ -28,13 +28,12 @@ if( isset( $_POST['template-contactform-submit'] ) AND $_POST['template-contactf
             $mail->AddAddress( $toemail , $toname );
             $mail->Subject = $subject;
 
-            $name = isset($name) ? "Name: $name<br><br>" : '';
+            $name = isset($name) ? "Naam: $name<br><br>" : '';
             $email = isset($email) ? "Email: $email<br><br>" : '';
-            $phone = isset($phone) ? "Phone: $phone<br><br>" : '';
-            $service = isset($service) ? "Service: $service<br><br>" : '';
-            $message = isset($message) ? "Message: $message<br><br>" : '';
+            $phone = isset($phone) ? "Telefoonnummer: $phone<br><br>" : '';
+            $message = isset($message) ? "Bericht: $message<br><br>" : '';
 
-            $referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
+            $referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>Dit bericht was verzonden van:: ' . $_SERVER['HTTP_REFERER'] : '';
 
             $body = "$name $email $phone $service $message $referrer";
 
@@ -42,7 +41,7 @@ if( isset( $_POST['template-contactform-submit'] ) AND $_POST['template-contactf
             $sendEmail = $mail->Send();
 
             if( $sendEmail == true ):
-                echo 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
+                echo 'We have <strong>successfully</strong> received your Message and will try to get back at you within 72 hours';
             else:
                 echo 'Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '';
             endif;
